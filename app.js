b88207c74459b1595fbe2e2d5d518e50f519880f
Ethan Swafford <ethanswafford@yahoo.com>
2026-05-04 22:52:52 -0400

    buildStarted

---
+++ app.js
@@ -0,0 +1,14 @@
+// calling server components
+var http = require('http');
+var express = require('express');
+var url = require('url');
+var path = require('path');
+var net = require('net');
+
+const app = express();
+
+// port used to open server
+const port = 4443;
+
+// Developer tests
+console.log('TEST CONTROLLER');
